0b569b6e8c0f724aaaf50ee8a99eea22a6138583
zemiaozzz <zemiaozhu@outlook.com>
2018-06-10 10:58:27 -0700

    #136 #137

---
+++ zemiao/137.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ans = 0
+        for i in xrange(0,32):
+            
+            # After this step, bit i of ans will be count(where bit i is 1) %3
+            count = 0
+            for a in nums:
+                if ((a >> i) & 1):
+                    count+=1
+            ans |= ((count%3) << i)
+            
+        return self.convert(ans)
+    
+    '''converts 64bit ans into 32bit,
+    x >= 2**31 means first bit is 1, a neg number.
+    neg number is stored as flip(pos)+1
+    '''
+    def convert(self,x):
+        if x >= 2**31:
+            x -= 2**32
+        return x
