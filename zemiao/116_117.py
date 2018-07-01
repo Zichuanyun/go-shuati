3ecea048e8064bbe3c4da29a3e12597e5fe1135d
zemiaozzz <zemiaozhu@outlook.com>
2018-07-01 13:37:01 -0700

    #116 #117

---
+++ zemiao/116_117.py
@@ -0,0 +1,27 @@
+# Definition for binary tree with next pointer.
+# class TreeLinkNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+#         self.next = None
+
+class Solution:
+    # @param root, a tree link node
+    # @return nothing
+    def connect(self, root):
+        if not root:
+            return
+        childrenLayerStart = TreeLinkNode(0);
+        while root:
+            childrenLayerStart.next = None
+            tmp = childrenLayerStart
+            while root:
+                if root.left:
+                    tmp.next = root.left
+                    tmp = tmp.next
+                if root.right:
+                    tmp.next = root.right
+                    tmp = tmp.next
+                root = root.next
+            root = childrenLayerStart.next
