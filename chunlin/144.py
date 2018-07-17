6cdd514f3db47bc87512b791b0d97b1180cb468c
Chun Lin <lc32402@gmail.com>
2018-07-16 22:48:14 -0700

    Create 144.py

---
+++ chunlin/144.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        # forgot edge case
+        if not root:
+            return list()
+        stack = list()
+        result = list()
+        stack.append(root)
+        while stack:
+            cur = stack.pop()
+            result.append(cur.val)
+            if cur.right:
+                stack.append(cur.right)
+            if cur.left:
+                stack.append(cur.left)
+        return result
