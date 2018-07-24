34ca7f892ea9f480fc96c85b120520e5e116badb
Tiannan Zhou <zhou0745@umn.edu>
2018-07-23 23:51:22 -0700

    Create 275.cpp

---
+++ tnzhou/275.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        if (citations.size() == 0)
+            return 0;
+        int res;
+        int left = 0;
+        int right = citations.size();
+        while (left <= right) {
+            int m = (left + right) / 2;
+            if (citations[m] >= citations.size() - m) {
+                res = citations.size() - m;
+                right = m - 1;
+            } else {
+                left = m + 1 ;
+            }
+        }
+        return res;
+    }
+};
